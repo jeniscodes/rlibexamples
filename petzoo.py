2672169147941d22c90ffee1e6142783d3943051
unknown <jenisht1@gmail.com>
2022-05-02 21:06:55 +0200

    petzoo

---
+++ petzoo.py
@@ -0,0 +1,9 @@
+from ray import tune
+from ray.rllib.models import ModelCatalog
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.tune.registry import register_env
+from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
+from pettingzoo.butterfly import pistonball_v5
+import supersuit as ss
+import torch
+from torch import nn

1e9f2e90edf0c48c3d935bf52841ecbede916829
unknown <jenisht1@gmail.com>
2022-05-03 14:42:56 +0200

    cartpole problem in rlib

--- petzoo.py
+++ petzoo.py
@@ -1,9 +1,18 @@
+import ray
+
+ray.init()
+
 from ray import tune
-from ray.rllib.models import ModelCatalog
-from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
-from ray.tune.registry import register_env
-from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
-from pettingzoo.butterfly import pistonball_v5
-import supersuit as ss
-import torch
-from torch import nn
+
+tune.run("PPO",
+            config={'env' : "CartPole-v1",
+                    "evaluation_interval" : 2, #num of training iter betn evaluations
+                    "evaluation_num_episodes" : 20,
+                    "num_gpus" : 1, 
+
+            
+            
+            }
+
+
+)
